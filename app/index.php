40f8e0d5c2308203ea9c27903ec915a231afb3ed
Alan <alan.rochford@mydit.ie>
2016-03-27 00:12:08 +0000

    Special Days working

--- app/index.php
+++ app/index.php
@@ -96,57 +96,99 @@ $app->map ( "/video/", function ($elementID = null) use ($app)
 {
 	$routeTime = $app->request()->get('time');
 	$currTime = $app->request()->get('currtime');
+	$currDay = $app->request()->get('currday');
+	$currMonth = $app->request()->get('currmonth');
+	$currDate = $app->request()->get('currdate');
 	$timeInt = intval($routeTime);
 	
-	switch ($currTime){
-		
-		case '00': case '01': case '02':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";			
-			break;
+	if($currDate == 17 && $currMonth == '03') // If St. Patricks Day--Play Irish Ballads
+	{
+		$sql = "SELECT video_key FROM video WHERE (genre = 'Irish')";	
+	}
+	
+	else if($currDate == 25 && $currMonth == '12')
+	{
+		$sql = "SELECT video_key FROM video WHERE (genre = 'Christmas')";
+	}
+	
+	else{
 			
-		case '03': case '04': case '05':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
+		switch ($currTime){
 			
-		case '06': case '07': case '08':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
+			case '00': case '01': case '02':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Soul') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500))
+						ORDER BY RANDOM() LIMIT 1";			
+				break;
+				
+			case '03': case '04': case '05':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Dance') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500))
+						ORDER BY RANDOM() LIMIT 1";
+				break;
+				
+			case '06': case '07': case '08':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Reggae' OR genre = 'Light Rock') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500))
+						ORDER BY RANDOM() LIMIT 1";
+				break;
+				
+			case '09': case '10': case '11':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Classical' OR genre = 'Light Rock') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500)) 
+						ORDER BY RANDOM() LIMIT 1";
+				break;
+				
+			case '12': case '13': case '14':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Country' OR genre = 'Classical') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500)) 
+						ORDER BY RANDOM() LIMIT 1";
+				break;
+				
+			case '15': case '16': case '17':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Indie' OR genre = 'Dance') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500 ))
+						ORDER BY RANDOM() LIMIT 1";
+				break;
+				
+			case '18': case '19': case '20':
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Rock' OR genre = 'HipHop' OR genre ='Techno') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500)) 
+						ORDER BY RANDOM() LIMIT 1";
+				break;
+				
+			case '21': case '22': case '23':
+				if($currDay == 'Fri')//If its Friday night play some upbeat party Music
+				{
+					$sql = "SELECT video_key FROM video WHERE (genre = 'Techno' OR genre = 'Rock' OR genre = 'HipHop') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500)) 
+						ORDER BY RANDOM() LIMIT 1";
+				}
+				else//If any other day play music chilled music
+				{
+				$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock' OR genre = 'Soul') 
+						AND (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500)) 
+						ORDER BY RANDOM() LIMIT 1";
+				}
+				break;
+				
 			
-		case '09': case '10': case '11':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
-			
-		case '12': case '13': case '14':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
-			
-		case '15': case '16': case '17':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
-			
-		case '18': case '19': case '20':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
-			
-		case '21': case '22': case '23':
-			$sql = "SELECT video_key FROM video WHERE (genre = 'Jazz' OR genre = 'Light Rock') 
-					AND (video_time BETWEEN ($routeTime - 100) AND ($routeTime + 100))";
-			break;
-			
-		
+		}
 	}
 
 	try {
 		$db = getDB();
 		$stmt = pg_query($db, $sql);
 		$arrayOfResults = fetchResults ( $stmt );
+		
+		if($arrayOfResults == null)//If no videos of that genre match route time, choose from all genres
+		{
+			$sql1 = "SELECT video_key FROM video WHERE (video_time BETWEEN ($routeTime - 500) AND ($routeTime + 500)) 
+					ORDER BY RANDOM() LIMIT 1";
+			$stmt = pg_query($db, $sql1);
+			$arrayOfResults = fetchResults ( $stmt );
+			
+		}
 		
 		$db = null;
 		echoRespnse(200, $arrayOfResults);
