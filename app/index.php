bb2bcc7bddd0d35b8c37032b25306b8a9d1f3127
Alan <alan.rochford@mydit.ie>
2016-04-05 04:15:59 +0100

    Distance corrected Again

--- app/index.php
+++ app/index.php
@@ -64,7 +64,7 @@ $app->map ( "/pubroutes/", function ($elementID = null) use ($app)
 	
 	$geom = geoPHP::load("POINT('$paramValue')");
 	$insert_string = pg_escape_bytea($geom->out('ewkb'));
-	$toReplace = "%20";
+	$toReplace = "%2520";
 	$locFormat = str_replace($toReplace, " ", $paramValue);
 	$sql = "SELECT route_name,route_time, visibility, ST_AsEWKT(geom) as geom,  
 			ST_Distance(ST_PointN(ST_GeomFromText(ST_AsEWKT(geom)),2)," . "'POINT(" . $locFormat . ")'" .") as distance 
